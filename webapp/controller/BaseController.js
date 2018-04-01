01933862f968f3e7faf4e4a461974f471909a61d
P1942051505 <pinaki.patra.pro@gmail.com>
2018-04-01 16:54:29 +0000

    Merge Functionality Changes

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -1,11 +1,23 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+], function (Controller) {
 	"use strict";
 
-	var baseController = Controller.extend("pinaki.sap.com.ApiClient.controller.BaseController", {});
-	baseController.pressShareButton = function(oEvent) {
-		console.log('hi');
-	};
-	return baseController;
+	return Controller.extend("pinaki.sap.com.ApiClient.controller.BaseController", {
+		
+		callFuncImp : function(oModel,method,path,urlParameters){
+			return new Promise(function(resolve,reject){
+				oModel.callFunction(path, {
+							method: method,
+							urlParameters: urlParameters,
+							success: function(oData, response) {
+								resolve(oData,response);
+							},
+							error: function(oError) {
+								reject(oError);
+							}
+						});
+			}.bind(this));
+		}
+	});
 });
--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,15 +1,24 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"pinaki/sap/com/ApiClient/controller/BaseController"
+], function(Controller, JSONModel, BaseController) {
 	"use strict";
-
-	return Controller.extend("pinaki.sap.com.ApiClient.controller.Home", {
-		onHistoryPress : function(){
+	var oData = {
+		// Crenentials- HCP Credentials
+		"mainlUrl": "htt"+"ps://hcpms-p1942051505trial.hanatrial.ondemand.com/SampleServices/ESPM.svc"
+	};
+	var oConfigModel = new JSONModel(oData);
+	return BaseController.extend("pinaki.sap.com.ApiClient.controller.Home", {
+		onAfterRendering : function(){
+			this.getView().setModel(oConfigModel,'idConfigModel');
+		},
+		toggleMasterPanelVisibility: function() {
 			var app = sap.ui.getCore().byId('__xmlview0--idSplitApp');
 			var mode = app.getMode();
-			if(mode === 'HideMode'){
+			if (mode === 'HideMode') {
 				app.setMode('PopoverMode');
-			}else{
+			} else {
 				app.setMode('HideMode');
 			}
 		}
