48df1c718fe8ec40c12423143721568d85570d37
P1942051505 <pinaki.patra.pro@gmail.com>
2018-04-01 20:41:35 +0000

    Add Chart JS

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -36,8 +36,13 @@ sap.ui.define([
 				});
 			}.bind(this));
 		},
-		initLocalStorage: function() {
-
+		loadJSFile : function(filename) {
+			var fileref = document.createElement('script');
+			fileref.setAttribute("type", "text/javascript");
+			fileref.setAttribute("src", filename);
+			if (typeof fileref != "undefined"){
+				document.getElementsByTagName("head")[0].appendChild(fileref);
+			}
 		}
 	});
 });
--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -6,7 +6,8 @@ sap.ui.define([
 	"use strict";
 	var oData = {
 		// Crenentials- HCP Credentials
-		"mainUrl": "htt" + "ps://hcpms-p1942051505trial.hanatrial.ondemand.com/SampleServices/ESPM.svc"
+		"mainUrl": "htt" + "ps://hcpms-p1942051505trial.hanatrial.ondemand.com/SampleServices/ESPM.svc",
+		"metaDataLoaded": false
 	};
 	var oConfigModel = new JSONModel(oData);
 	return BaseController.extend("pinaki.sap.com.ApiClient.controller.Home", {
@@ -15,9 +16,10 @@ sap.ui.define([
 		},
 		onAfterRendering: function() {
 			this.getView().setModel(oConfigModel, 'idConfigModel');
+			this.loadJSFile("https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js");
 		},
-		toggleMasterPanelVisibility: function() {
-			var app = sap.ui.getCore().byId('__xmlview0--idSplitApp');
+		toggleMasterPanelVisibility: function(oEvent) {
+			var app = oEvent.getSource().getParent().getParent().getParent().getParent().getParent();
 			var mode = app.getMode();
 			if (mode === 'HideMode') {
 				app.setMode('PopoverMode');
@@ -39,6 +41,8 @@ sap.ui.define([
 		serviceInitialized: function(oMainModel) {
 			oMainModel.attachMetadataLoaded(function() {
 				this.metaLoadPerformance();
+				oConfigModel.setProperty("/metaDataLoaded", true);
+				this.initAnalytics();
 				oConfigModel.setData({
 					metadata: oMainModel.getServiceMetadata().dataServices.schema[0].entityContainer[0]
 				}, true);
@@ -58,10 +62,40 @@ sap.ui.define([
 			var aHistory = this.getView().getModel('idLocalStoreModel').getProperty('/metadataLoad/history');
 			aHistory.push({
 				"duration": measures[measures.length - 1].duration,
-				"datetime": new Date().toLocaleTimeString(),
+				"datetime": new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString(),
 				"url": oConfigModel.getProperty('/mainUrl')
 			});
 			localStorage.setItem('analytics', JSON.stringify(this.getView().getModel('idLocalStoreModel').getData()));
+		},
+		initAnalytics: function() {
+			var aHistory = this.getView().getModel('idLocalStoreModel').getProperty('/metadataLoad/history');
+			var aMeasures=[] , aDimension=[];
+			for(var i=aHistory.length - 1;i>-1;i--){
+				if(aHistory[i].url === oConfigModel.getProperty('/mainUrl')){
+					aMeasures.push(aHistory[i].duration);
+					aDimension.push(aHistory[i].datetime);
+				}
+				if( aDimension.length === 20){
+					break;
+				}
+			}
+			this.loadChart('line',aMeasures.reverse(),aDimension.reverse());
+		},
+		loadChart: function(type,aMeasures,aDimension) {
+			var ctx = document.getElementById(this.getView().byId('metaLoadJSChart').getId()).getContext('2d');
+			var chart = new Chart(ctx, {
+				type: type,
+				data: {
+					labels: aDimension,
+					datasets: [{
+						label: "History of metadata load time(Miliseconds) for "+oConfigModel.getProperty('/mainUrl'),
+						backgroundColor: 'rgb(255, 99, 132)',
+						borderColor: 'rgb(255, 99, 132)',
+						data: aMeasures
+					}]
+				},
+				options: {}
+			});
 		}
 	});
 });
