087f90eea7807bb65e8e310510c5a6ce756ed3ca
P1942051505 <pinaki.patra.pro@gmail.com>
2018-04-01 19:23:02 +0000

    Add local Storage for history

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -1,23 +1,43 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("pinaki.sap.com.ApiClient.controller.BaseController", {
-		
-		callFuncImp : function(oModel,method,path,urlParameters){
-			return new Promise(function(resolve,reject){
+		initializeLocalData: function(that) {
+			//Initialize Local storage
+			var localStoreData = {
+				"metadataLoad": {
+					"current": "",
+					"history": []
+				}
+			};
+			if (localStorage.getItem("analytics")) {
+				var oLocalModel = new JSONModel(JSON.parse(localStorage.getItem('analytics')));
+				that.getView().setModel(oLocalModel, 'idLocalStoreModel');
+			} else {
+				var oLocalModel = new JSONModel(localStoreData);
+				that.getView().setModel(oLocalModel, 'idLocalStoreModel');
+				localStorage.setItem('analytics', JSON.stringify(localStoreData));
+			}
+		},
+		callFuncImp: function(oModel, method, path, urlParameters) {
+			return new Promise(function(resolve, reject) {
 				oModel.callFunction(path, {
-							method: method,
-							urlParameters: urlParameters,
-							success: function(oData, response) {
-								resolve(oData,response);
-							},
-							error: function(oError) {
-								reject(oError);
-							}
-						});
+					method: method,
+					urlParameters: urlParameters,
+					success: function(oData, response) {
+						resolve(oData, response);
+					},
+					error: function(oError) {
+						reject(oError);
+					}
+				});
 			}.bind(this));
+		},
+		initLocalStorage: function() {
+
 		}
 	});
 });
--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -6,12 +6,15 @@ sap.ui.define([
 	"use strict";
 	var oData = {
 		// Crenentials- HCP Credentials
-		"mainlUrl": "htt"+"ps://hcpms-p1942051505trial.hanatrial.ondemand.com/SampleServices/ESPM.svc"
+		"mainUrl": "htt" + "ps://hcpms-p1942051505trial.hanatrial.ondemand.com/SampleServices/ESPM.svc"
 	};
 	var oConfigModel = new JSONModel(oData);
 	return BaseController.extend("pinaki.sap.com.ApiClient.controller.Home", {
-		onAfterRendering : function(){
-			this.getView().setModel(oConfigModel,'idConfigModel');
+		onInit: function() {
+			this.initializeLocalData(this);
+		},
+		onAfterRendering: function() {
+			this.getView().setModel(oConfigModel, 'idConfigModel');
 		},
 		toggleMasterPanelVisibility: function() {
 			var app = sap.ui.getCore().byId('__xmlview0--idSplitApp');
@@ -21,6 +24,44 @@ sap.ui.define([
 			} else {
 				app.setMode('HideMode');
 			}
+		},
+		onLoadUrlPress: function() {
+			var url = oConfigModel.getProperty('/mainUrl');
+			performance.mark("requestSent");
+			var oModel = new sap.ui.model.odata.v2.ODataModel(url, {
+				headers: {
+					"sap-stastics": "true"
+				}
+			});
+			oModel.attachMetadataLoaded(this.serviceInitialized(oModel));
+			this.getView().setModel(oModel);
+		},
+		serviceInitialized: function(oMainModel) {
+			oMainModel.attachMetadataLoaded(function() {
+				this.metaLoadPerformance();
+				oConfigModel.setData({
+					metadata: oMainModel.getServiceMetadata().dataServices.schema[0].entityContainer[0]
+				}, true);
+			}.bind(this));
+		},
+		metaLoadPerformance: function() {
+			performance.mark("requestCompleted");
+			performance.measure(
+				"metadataLoad",
+				"requestSent",
+				"requestCompleted"
+			);
+			var measures = performance.getEntriesByName("metadataLoad");
+			this.getView().getModel('idLocalStoreModel').setProperty('/metadataLoad/current', {
+				"duration": measures[measures.length - 1].duration
+			}, true);
+			var aHistory = this.getView().getModel('idLocalStoreModel').getProperty('/metadataLoad/history');
+			aHistory.push({
+				"duration": measures[measures.length - 1].duration,
+				"datetime": new Date().toLocaleTimeString(),
+				"url": oConfigModel.getProperty('/mainUrl')
+			});
+			localStorage.setItem('analytics', JSON.stringify(this.getView().getModel('idLocalStoreModel').getData()));
 		}
 	});
 });
