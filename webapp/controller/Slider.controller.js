57006067181c0175f973dc080defb667a1a9d916
C5262685 <pinaki.patra@sap.com>
2018-04-08 18:14:45 +0000

    Add History Pane List -- Needs to be modified

--- webapp/controller/Slider.controller.js
+++ webapp/controller/Slider.controller.js
@@ -1,9 +1,12 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"pinaki/sap/com/ApiClient/controller/BaseController"
+], function(Controller,BaseController) {
 	"use strict";
 
-	return Controller.extend("pinaki.sap.com.ApiClient.controller.Slider", {
-
+	return BaseController.extend("pinaki.sap.com.ApiClient.controller.Slider", {
+		onInit: function() {
+			this.initializeLocalData(this);
+		}
 	});
 });
