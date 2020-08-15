51533972b8208a725937296ce7be479e0fdcc418
Pinaki Patra <pinaki.patra@gds.ey.com>
2020-08-15 23:58:54 +0530

    Update The nav to home and nav back

--- webapp/controller/SplitApp.controller.js
+++ webapp/controller/SplitApp.controller.js
@@ -1,9 +1,24 @@
 sap.ui.define([
+	"sap/ui/core/routing/History",
 	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+], function (History, Controller) {
 	"use strict";
 
 	return Controller.extend("pinaki.sap.com.ApiClient.controller.SplitApp", {
+		navBack: function () {
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
 
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				var oRouter = UIComponent.getRouterFor(this);
+				oRouter.navTo("Home");
+			}
+		},
+		navToHome : function(){
+			var oRouter = UIComponent.getRouterFor(this);
+				oRouter.navTo("Home");
+		}
 	});
 });
